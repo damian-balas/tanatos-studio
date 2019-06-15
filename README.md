7ac4cfe4ea2266137fd3a440a3baa4f8a8b74377
Damian Balas <damian.webdev@gmail.com>
2019-06-15 05:06:38 +0200

    Finished project v3 - improvements

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# tanatos-studio
+# **Tanatos Studio**
+
+Tanatos Studio is a simple static website based on free cymu PSD template. 
+Made for training purposes :smirk:. 
+
+
+
+I hope You :thumbsup: it c:
+
+___
+
+[**Tanatos Studio**](https://damian-balas.github.io/tanatos-studio/)
+
+___
+
+
+![Tanatos Studio preview](https://imgur.com/1lWh02R.jpg)
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,39 +1,51 @@
-document.addEventListener("DOMContentLoaded", function(){
 
-  //parallax landing elements
-  let imageContainer = document.querySelector('.our-works__images'),
-      images = document.querySelectorAll('.our-works__image');
+//parallax landing elements
+const imageContainer = document.querySelector('.our-works__images');
+const images = document.querySelectorAll('.our-works__image');
 
-  window.addEventListener('scroll', () => {
-    let wScroll = window.pageYOffset;
+window.addEventListener('scroll', () => {
+  const wScroll = window.pageYOffset;
 
-    if(wScroll > imageContainer.offsetTop - window.innerHeight / 1.25){
-      images.forEach((workImage,index) => {
-        setTimeout(() => {
-          workImage.classList.add('is-showing');
-        }, 150 * (index+1));
-      });
-    }
-  });
-
-  //current year in the footer
-  function currentYear(){
-    let date =  new Date();
-    let year = date.getFullYear();
-    document.querySelector('.site-footer__year').innerHTML = year;
-   
+  if(wScroll > imageContainer.offsetTop - window.innerHeight / 1.25){
+    images.forEach((workImage,index) => {
+      setTimeout(() => {
+        workImage.classList.add('is-showing');
+      }, 150 * (index+1));
+    });
   }
-  currentYear();
-
-  //swiper init
-  let mySwiper = new Swiper ('.swiper-container', {
-    direction: 'horizontal',
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    }
-  })
 });
+
+//current year in the footer
+function updateCurrentYear(){
+  const date =  new Date();
+  const year = date.getFullYear();
+  document.querySelector('.site-footer__year').innerHTML = year;
+}
+updateCurrentYear();
+
+//closing navigation
+
+const navItems = document.querySelectorAll('.site-nav__a');
+const checkbox = document.querySelector('#site-nav-toggle');
+
+navItems.forEach((i) => {
+  i.addEventListener('click', ()=>{
+    checkbox.checked = false;
+  });
+});
+
+
+
+
+//swiper init
+let mySwiper = new Swiper ('.swiper-container', {
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
+
 
 
